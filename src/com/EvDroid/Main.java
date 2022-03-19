11f33c4405195d265f7df012de45c744beaf460d
EvVlF <ev.vl.fedorov@gmail.com>
2022-03-19 14:14:08 +0400

    Mah homework.

---
+++ src/com/EvDroid/Main.java
@@ -0,0 +1,26 @@
+package com.EvDroid;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + ", " + world + "!");
+        int a = 1;
+        int b = 2;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        int c = a + b;
+        System.out.println("sum= " + c);
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");
+    }
+}
